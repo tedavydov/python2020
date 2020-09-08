81e55ab3f55d439ed66d3e9967bc9b31b4d29408
TimDV_LeJet2 <timdv@yandex.ru>
2020-09-08 20:57:07 +0300

    Выполнение задания 5 :  Запросите у пользователя значения  выручки и издержек фирмы.  Определите,    с каким финансовым результатом работает фирма    (  прибыль — выручка больше издержек,    или убыток — издержки больше выручки )  Выведите соответствующее сообщение.
     Если фирма отработала с прибылью,
     вычислите рентабельность выручки
     (соотношение прибыли к выручке)
    
     Далее запросите численность сотрудников фирмы
     и определите прибыль фирмы
     в расчете на одного сотрудника.

---
+++ Lesson1_DZ5.py
@@ -0,0 +1,24 @@
+vyr = float(input("Введите значение выручки фирмы "))
+izd = float(input("Введите значение издержек фирмы "))
+
+rent = 1
+rent_str = ""
+result = ""
+if vyr > izd:
+    result = "прибыль"
+    rent = (vyr - izd) / vyr
+    rent_str = f"рентабельность равна {rent}"
+elif vyr < izd:
+    result = "убыток"
+else:
+    result = "хм .. а не видно пока результатов - товарищи !"
+
+print(f"Результат работы вашей фирмы : {result}, а {rent_str}")
+
+if rent != 1:
+    sotr_count = int(input("А введите-ка количество сотрудников вашей фирмы "))
+    if sotr_count <= 0:
+        print("Да вы никак обнуть нас хотите ?")
+    else:
+        print(f"А ведь на каждого сотрудника вашей фирмы приходится : {(vyr - izd) / sotr_count} рублей прибыли !")
+
