48cdfa9823a7242740bdc196252fece927992ab4
TimDV_LeJet2 <timdv@yandex.ru>
2020-09-08 18:21:56 +0300

    Выполнение задания 1 :  исправил ошибку ValueError: invalid literal for int() ..  - отказался от преобразования в число неизвестной заранее строки  !!! нужно узнать как проверять значения до преобразования,  или дождаться темы по исключениям ..

--- Lesson1_DZ1.py
+++ Lesson1_DZ1.py
@@ -1,10 +1,15 @@
-temp_str = "строка для проверки"
+temp_str = "стр"
 temp_int = 7
 temp_usr = input("Угадайте что я написал ! Введите что-нибудь .. ")
 if temp_usr == temp_str:
     print("УРА ! Вы угадали строку : ", temp_str)
-elif temp_usr == temp_int:
-    print("Вы угадали число : ", temp_int)
+    # temp_int2 = int(temp_usr)
+    # print("int(temp_usr) : ", type(int(temp_usr))) # ValueError: invalid literal for int() ..
+    # print ("temp_int : ", type(temp_int))
+    # print("temp_str : ", type(temp_str))
+    # print("temp_usr : ", type(temp_usr))
+# elif int(temp_usr) == temp_int:  # ValueError: invalid literal for int() ..
+#     print("Вы угадали число : ", temp_int)
 else:
     print("Вы не угадали.")
 
