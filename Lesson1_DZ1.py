ec773053b0576077715daa653a7fe043d28fd6bb
TimDV_LeJet2 <timdv@yandex.ru>
2020-09-08 17:25:21 +0300

    Выполнение задания 1 :  Поработайте с переменными, создайте несколько, выведите на экран, запросите у пользователя несколько чисел и строк и сохраните в переменные, выведите на экран

--- Lesson1_DZ1.py
+++ Lesson1_DZ1.py
@@ -1,3 +1,10 @@
 temp_str = "строка для проверки"
 temp_int = 7
 temp_usr = input("Угадайте что я написал ! Введите что-нибудь .. ")
+if temp_usr == temp_str:
+    print("УРА ! Вы угадали строку : ", temp_str)
+elif temp_usr == temp_int:
+    print("Вы угадали число : ", temp_int)
+else:
+    print("Вы не угадали.")
+
