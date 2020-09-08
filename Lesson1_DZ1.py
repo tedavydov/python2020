ff0985ed50346a74c12ea79ed6ea14c4df25d469
TimDV_LeJet2 <timdv@yandex.ru>
2020-09-08 18:46:31 +0300

    Выполнение задания 1 :  дописал работу с числовой переменной

--- Lesson1_DZ1.py
+++ Lesson1_DZ1.py
@@ -1,15 +1,14 @@
 temp_str = "стр"
-temp_int = 7
-temp_usr = input("Угадайте что я написал ! Введите что-нибудь .. ")
+temp_usr = input("Угадайте что я написал ! Введите строку .. ")
 if temp_usr == temp_str:
     print("УРА ! Вы угадали строку : ", temp_str)
-    # temp_int2 = int(temp_usr)
-    # print("int(temp_usr) : ", type(int(temp_usr))) # ValueError: invalid literal for int() ..
-    # print ("temp_int : ", type(temp_int))
-    # print("temp_str : ", type(temp_str))
-    # print("temp_usr : ", type(temp_usr))
-# elif int(temp_usr) == temp_int:  # ValueError: invalid literal for int() ..
-#     print("Вы угадали число : ", temp_int)
+else:
+    print("Вы не угадали.")
+
+temp_int = 7
+temp_usr = int(input("Угадайте что я загадал ! Введите число .. "))
+if temp_usr == temp_int:
+    print("УРА ! Вы угадали число : ", temp_int)
 else:
     print("Вы не угадали.")
 
