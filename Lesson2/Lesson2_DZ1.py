473290da09f38d9e24418e29b42f9f95dd2acb43
Teymuraz Davydov <timdv@yandex.ru>
2020-09-14 00:21:19 +0300

    Урок 2 задание 1 : доработка      Дописал работу с bool и list

--- Lesson2/Lesson2_DZ1.py
+++ Lesson2/Lesson2_DZ1.py
@@ -1,7 +1,10 @@
-type_list = ["int", "float", "str", "bin", "oct", "hex"]
-my_list = [1, 2.5, "str", 0b1011000111001, 0o22, 0x1C]
-
+type_list = ["bool", "int", "float", "str", "bin", "oct", "hex", "list"]
+my_list = [False, 1, 2.5, "str", 0b1011000111001, 0o22, 0x1C, [1, 3]]
 us_list = []
+
+us_el = bool(input("Введите логическое значение True/False "))
+us_list.append(us_el)
+
 us_el = int(input("Введите целое десятичное число  "))
 us_list.append(us_el)
 
@@ -26,11 +29,14 @@ us_el = hex(int(us_el, 16))
 # print(f"{type(us_el)}  {us_el}")
 us_list.append(us_el)
 
+input_list = input("Введите ряд значений, разделяя из пробелами :  ").split()
+us_list.append(input_list)
+
 # не получилось добавить кортеж в список ..
 # us_list.append(input("Введите несколько значений через запятую"))
 
 print("У нас есть эталонный список значений : ")
-print("list = [1, 2.5, 'str', 0b1011000111001, 0o22, 0x1C]")
+print("list = [False, 1, 2.5, 'str', 0b1011000111001, 0o22, 0x1C, [1, 3]]")
 print(f"в памяти он сохранился как : {my_list}")
 
 print("Сравните введенные вами значения с нашим списком : ")
@@ -38,8 +44,8 @@ for us in us_list:
     index = us_list.index(us)
     type_my = str(type(my_list[index]))
     type_us = str(type(us))
-    print(f"{type_list[index]:<7}=>  {us:<11} is {type_us:>16}"
-          f"      example: {my_list[index]:<5} is {type_my}")
+    print(f"{type_list[index]:<7}=>  {str(us):>15} is {type_us:>16}"
+          f"      example: {str(my_list[index]):>10} is {type_my:>16}")
 
 # РЕЗУЛЬТАТ :
 #
