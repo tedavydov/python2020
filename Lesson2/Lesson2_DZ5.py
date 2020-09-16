de1fa6ea8d09ca857e6752c452e61b0986ac2437
TimDV_LeJet2 <timdv@yandex.ru>
2020-09-16 05:03:46 +0300

    Урок 2 - Выполнение задания 1 :  Переделал подсветку рейтинга !
             Теперь подсветка формируется не циклом перебора
             а f-строкой и слайсами списка (рейтинга) ..
             ну и добавил обработку ошибок ввода (исключения из урока 3)
    
    ЗАДАНИЕ :
        Создать список и заполнить его элементами различных типов данных.
        Реализовать скрипт проверки типа данных каждого элемента.
        Использовать функцию type() для проверки типа.
    
    Элементы списка можно не запрашивать у пользователя, а указать явно, в программе.

--- Lesson2/Lesson2_DZ5.py
+++ Lesson2/Lesson2_DZ5.py
@@ -5,10 +5,14 @@ num = 0
 num_str = " "
 while num_str:  # пока не введена пустая строка
     num_str = input("Сколько вы набрали очков ?  ")
-    if num_str:
-        num = int(num_str)
-    else:
-        print("Спасибо ! Вы хорошо играли !")
+    try:
+        if num_str:
+            num = int(num_str)
+        else:
+            print("Спасибо ! Вы хорошо играли !")
+            break
+    except:
+        print("Вы ввели не целое число ! Игра окончена !")
         break
     i = 0
     j = 0
@@ -22,20 +26,47 @@ while num_str:  # пока не введена пустая строка
             i += 1
         rating.insert(j, num)  # вставим новый элемент по индексу
 
+    # ВЫВОД В F-СТРОКЕ
     # сформируем вывод списка с выделением места нового элемента
-    print("Ваше \033[31m" + "место\033[0m " + "в рейтинге : [", end="")
-    i = 0
-    for el in rating:
-        if i != j:
-            print(f"{rating[i]}", end="")
-        else:
-            print("\033[31m" + f"{rating[j]}" + "\033[0m", end="")  # выделение цветом
-            # print(f"({rating[j]})", end="") # выделение скобками
-        if i < rating_len:
-            print(", ", end="")
-        else:
-            print("", end="")
-        i += 1
-    print("]")
+    # ( новый вариант с f-строкой и слайсом )
+    if j == 0:
+        print("Ваше \033[31m" + "место\033[0m " + "в рейтинге : ["
+              + "\033[31m" + f"{rating[j]}" + "\033[0m"
+              + f", {str(rating[1:]).strip('[]')}]")
+        # print(f"DEBUG:  j == {j} (0)")
+    elif j == rating_len:
+        print("Ваше \033[31m" + "место\033[0m " + "в рейтинге : ["
+              + f"{str(rating[:rating_len]).strip('[]')}, "
+              + "\033[31m" + f"{rating[j]}" + "\033[0m]")
+        # print(f"DEBUG: j == {j} (rating_len == {rating_len})")
+    else:
+        print("Ваше \033[31m" + "место\033[0m " + "в рейтинге : ["
+              + f"{str(rating[:j]).strip('[]')}, "
+              + "\033[31m" + f"{rating[j]}" + "\033[0m"
+              + f", {str(rating[j + 1:]).strip('[]')}]")
+    #     print(f"DEBUG: j == {j} in {rating_len}")
+    #     print(str(rating[:j]))
+    #     print(str(rating[j + 1:]))
+    #     print("=" * 16)
+    # print(f"DEBUG: rating == {rating}")
+    # print("=" * 20)
 
-# print(rating)
+    # # ВЫВОД В ЦИКЛЕ ПЕРЕБОРА
+    # # сформируем вывод списка с выделением места нового элемента
+    # # ( старый вариант с циклом перебора индексов .. )
+    # print("Ваше \033[31m" + "место\033[0m " + "в рейтинге : [", end="")
+    # i = 0
+    # for el in rating:
+    #     if i != j:
+    #         print(f"{rating[i]}", end="")
+    #     else:
+    #         print("\033[31m" + f"{rating[j]}" + "\033[0m", end="")  # выделение цветом
+    #         # print(f"({rating[j]})", end="") # выделение скобками
+    #     if i < rating_len:
+    #         print(", ", end="")
+    #     else:
+    #         print("", end="")
+    #     i += 1
+    # # print("]")
+    # # print(f"DEBUG: rating == {rating}  j == {j} (rating_len == {rating_len})")
+    # # print("=" * 20)
