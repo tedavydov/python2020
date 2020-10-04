8a8e0051256a2ec92aa7ceecb045da16b63bdb62
Teymuraz Davydov <timdv@yandex.ru>
2020-10-04 15:32:45 +0300

    Урок 8 задание 1 :      Реализовать класс «Дата», функция-конструктор которого должна принимать дату в виде строки формата «день-месяц-год». В рамках класса реализовать два метода. Первый, с декоратором @classmethod, должен извлекать число, месяц, год и преобразовывать их тип к типу «Число». Второй, с декоратором @staticmethod, должен проводить валидацию числа, месяца и года (например, месяц — от 1 до 12). Проверить работу полученной структуры на реальных данных.

---
+++ Lesson8/Lesson8_DZ1.py
@@ -0,0 +1,121 @@
+class Data:
+
+    def __init__(self, data):
+        if data and isinstance(data, str):
+            self.__data_s = data
+            # думаю что конвертация и валидация должны проводится сразу при создании обьекта
+
+            # метод класса должен получить данные инициализации объекта
+            # print(f"CONVERT (00): self.__data_s = ({str(self.__data_s)})")
+            self.__convert_to_num(self)
+            # print(f"CONVERT (01): self.__data_s = ({str(self.__data_s)})")
+
+            # так метод класса не получит данные инициализации объекта
+            # self.__convert_to_num()
+
+            self.__date_validation(self)
+            # print(f"validation (02): self.__data_s = ({str(self.__data_s)})")
+            # но можно для чистоты эксперимента их вызывать отдельно
+
+        else:
+            self.__data_s = ""
+            print(f"ERROR: DATA is not a string ({str(data)})")
+
+    def __str__(self):
+        # print(f"__str__ (x1): self.__data_s = ({str(self.__data_s)})")
+        if self.__data_s:
+            try:
+                # print(f"__str__ (x2): self.__y/__m/__d = {self.__y}/{self.__m}/{self.__d}")
+                # print(f"__str__ (x3): self.__data_s = ({str(self.__data_s)})")
+                # print()
+
+                if self.__y and self.__m and self.__d:
+                    return f"Date: {self.__y}/{self.__m}/{self.__d}"
+                else:
+                    return f"Date string: {str(self.__data_s)}"
+            except:
+                return f"Obj ERROR: wrong date format {str(self.__data_s)}"
+        else:
+            return "Obj ERROR: date not set"
+
+    @classmethod
+    def __convert_to_num(cls, obj):
+        # print(f"CONVERT (0) @classmethod: obj.__data_s = ({str(obj.__data_s)})")
+        if obj.__data_s:
+            try:
+                if obj.__data_s.find(".") >= 0:
+                    tmp = map(int, obj.__data_s.split("."))
+                elif obj.__data_s.find("/") >= 0:
+                    tmp = map(int, obj.__data_s.split("/"))
+                elif obj.__data_s.find("-") >= 0:
+                    tmp = map(int, obj.__data_s.split("-"))
+                else:
+                    tmp = map(int, obj.__data_s.split())
+                obj.__y, obj.__m, obj.__d = tmp
+                # print(f"CONVERT Ok : date = ({str(obj.__y)}/{str(obj.__m)}/{str(obj.__d)})")
+                # cls.__y, cls.__m, cls.__d = [1901, 55, 7]  # тест 2 ( для сравнения )
+                # print(f"CONVERT 1901/55/7 : cls date = ({str(cls.__y)}/{str(cls.__m)}/{str(cls.__d)})")
+                # cls.__y, cls.__m, cls.__d = [1901, 5, 7]  # тест 1 ( для сравнения )
+                # print(f"CONVERT 1901/5/7 : cls date = ({str(cls.__y)}/{str(cls.__m)}/{str(cls.__d)})")
+                # cls.__y, cls.__m, cls.__d = tmp  # для теста ( как было с ошибкой месяц=13 в __str__ )
+                # print(f"CONVERT : cls date = ({str(cls.__y)}/{str(cls.__m)}/{str(cls.__d)})")
+            except:
+                print(f"CONVERT ERROR: wrong date format ({str(obj.__data_s)})")
+        else:
+            print("CONVERT ERROR: date not set")
+        # print(f"CONVERT (1) @classmethod: obj.__data_s = ({str(obj.__data_s)})")
+
+    @staticmethod
+    def __date_validation(self_obj):
+        try:
+            if self_obj.__y:
+                if self_obj.__y < 1900: self_obj.__y = 1900
+                if self_obj.__y > 2020: self_obj.__y = 2020
+                print(f"verified year = ({str(self_obj.__y)})")
+            else:
+                self_obj.__y = 1900
+                print("VALIDATION ERROR: empty year !")
+            if self_obj.__m:
+                if self_obj.__m < 1: self_obj.__m = 1
+                if self_obj.__m > 12: self_obj.__m = 12
+                print(f"verified month = ({str(self_obj.__m)})")
+            else:
+                self_obj.__m = 1
+                print("VALIDATION ERROR: empty month !")
+            if self_obj.__d:
+                if self_obj.__d < 1: self_obj.__d = 1
+                if self_obj.__m in [1, 3, 5, 7, 8, 10, 12] and self_obj.__d > 31:
+                    self_obj.__d = 31
+                elif self_obj.__m == 2 and not (self_obj.__y / 4) and self_obj.__d > 29:
+                    self_obj.__d = 29
+                elif self_obj.__m == 2 and (self_obj.__y / 4) and self_obj.__d > 28:
+                    self_obj.__d = 28
+                elif self_obj.__d > 30:
+                    self_obj.__d = 30
+                print(f"verified day = ({str(self_obj.__d)})")
+            else:
+                self_obj.__d = 1
+                print("VALIDATION ERROR: empty day !")
+        except:
+            print(f"VALIDATION ERROR: wrong date ({self_obj.__data_s})")
+
+
+print("=" * 10, "зададим 1-ю дату (23)", "=" * 25)
+date_1 = Data(23)
+print("=" * 10, "зададим 2-ю дату (23/12/2021)", "=" * 25)
+date_2 = Data("23/12/2021")
+print("=" * 10, "зададим 3-ю дату (2020.2.0)", "=" * 25)
+date_3 = Data("2020.2.0")
+print("=" * 10, "зададим 4-ю дату (1020-09-31)", "=" * 25)
+date_4 = Data("1020-09-31")
+print("=" * 10, "зададим 5-ю дату (2001/13/05)", "=" * 25)
+date_5 = Data("2001/13/05")
+
+print()
+print("=" * 10, "проверим что же мы насоздавали", "=" * 25)
+print(f"1: {date_1}\n2: {date_2}\n3: {date_3}\n4: {date_4}\n5: {date_5}")
+# print("1: ", date_1)
+# print("2: ", date_2)
+# print("3: ", date_3)
+# print("4: ", date_4)
+# print("5: ", date_5)
